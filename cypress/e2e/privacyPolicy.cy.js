83def54584f9fce19fcdd3b963d4e5b06ab7e3a6
rodrigovisse <rodriga7x1@gmail.com>
2025-08-11 22:52:10 -0300

    Break the app on purpose

---
+++ cypress/e2e/privacyPolicy.cy.js
@@ -0,0 +1,6 @@
+ it('testa a pagina da politica de privacidade de forma independente', () => {
+   cy.visit('./src/privacy.html')
+   cy.contains('h1', 'CAC TAT - Política de Privacidade').should('be.visible')
+   cy.contains('p', 'Talking About Testing')
+
+   })
