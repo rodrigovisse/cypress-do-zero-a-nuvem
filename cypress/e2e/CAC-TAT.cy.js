59d19a495c7ef9d8b6c885a2748b6eeb0a7f49eb
rodrigovisse <rodriga7x1@gmail.com>
2025-08-11 23:04:21 -0300

    Configure the ci workflow

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -174,8 +174,32 @@ it('marca o tipo de atendimento', () => {
     .selectFile('@sampleFile')
     .should((input) => {
       expect(input[0].files[0].name).to.equal('example.json')
+<<<<<<< HEAD
+=======
+      
+   })
+   })
+   //<a href="privacy.html" target="_blank">Política de Privacidade</a>
+   
+   it('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', () => {
+    cy.contains('a', 'Política de Privacidade')
+      .should('have.attr', 'href', 'privacy.html')
+      .and('have.attr', 'target', '_blank')
+>>>>>>> 5aefa28 (Configure the ci workflow)
    })
 
-})
+   it('acessa a página da política de privacidade removendo o target e então clicando no link', () => {
+     cy.contains('a', 'Política de Privacidade')
+      .invoke('removeAttr', 'target')
+      .click()
+
+      cy.contains('h1', 'CAC TAT - Política de Privacidade').should('be.visible')
+    
+   })
+
+
 
 })
+
+
+
--- cypress/e2e/privacyPolicy.cy.js
+++ cypress/e2e/privacyPolicy.cy.js
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
  it('testa a pagina da politica de privacidade de forma independente', () => {
+=======
+ it.only('testa a pagina da politica de privacidade de forma independente', () => {
+>>>>>>> 5aefa28 (Configure the ci workflow)
    cy.visit('./src/privacy.html')
    cy.contains('h1', 'CAC TAT - Política de Privacidade').should('be.visible')
    cy.contains('p', 'Talking About Testing')
