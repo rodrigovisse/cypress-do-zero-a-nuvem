9ca2bd292557082efd45e77dcddd3395db4af561
rodrigovisse <rodriga7x1@gmail.com>
2026-03-16 23:27:45 -0300

    Configure cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,10 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  projectId: "xuw8t2",
   viewportHeight: 880,
   viewportWidth: 1280,
   e2e: {},
   //video: true
 })
+
